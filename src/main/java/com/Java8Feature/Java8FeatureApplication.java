e57c8369125857626465a3a61531a07cd491d9dd
sagarmaheshri <sagarmaheshri007@gmail.com>
2026-02-01 19:03:11 +0530

    first commit

---
+++ src/main/java/com/Java8Feature/Java8FeatureApplication.java
@@ -0,0 +1,13 @@
+package com.Java8Feature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8FeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8FeatureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Java8Feature/Java8FeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Java8Feature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8FeatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
